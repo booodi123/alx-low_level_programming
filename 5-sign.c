f2cf6dc7badd939769bd1284777091168e81ab43
booodi123 <138764565+booodi123@users.noreply.github.com>
2023-08-16 00:17:50 +0200

    Update 5-sign.c

--- 5-sign.c
+++ 5-sign.c
@@ -1,24 +1,29 @@
 #include "main.h"
 /**
-*print_sign-prints sign of a number
-*@n: character to be verified
-*Return: return 0 or 1
-*/
+ * print_sign - prints the sign of the given number +/-
+ * @n: Number to be tested
+ *
+ *
+ * Return: 1 if positive and greater than zero, 0 if is Zero, and -1 if neg.
+ */
 int print_sign(int n)
 {
-if (n == 0)
-{
-_putchar('0');
-return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
-else if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else
-{
-_putchar('-');
-return (-1);
-}
+
+
 
